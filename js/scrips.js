9b55e2b028fb6bb677387d0fe4de77d7804ad18e
santiago306-jhon <jhonsanrodriguez20@gmail.com>
2025-11-26 13:20:49 -0500

    mas botones

--- js/scrips.js
+++ js/scrips.js
@@ -24,3 +24,21 @@ function cambiodeTexto(){
 function fondoAside(){
     document.getElementById('aside').style.background='#f584f5ff';
 }
+function alert1(){ 
+    alert("Este es el mensaje número 1");
+}
+function alert2(){ 
+    alert("Hola, este es el mensaje 2"); 
+}
+function alert3(){ 
+    alert("Probando alert 3, todo ok"); 
+}
+function alert4(){ 
+    alert("Mensaje 4 funcionando correctamente"); 
+}
+function alert5(){ 
+    alert("Mensaje 5: JS es divertido"); 
+}
+function alert6(){ 
+    alert("Mensaje 6: Último alert creado"); 
+}
